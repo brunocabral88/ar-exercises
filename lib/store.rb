14bbf42e702c2c5452f24d173914a20e874f474c
Bruno Cabral <bruno.cabral88@hotmail.com>
2017-04-11 20:07:32 +0000

    End of exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -1,13 +1,26 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
   validate :is_women_or_men_apparel
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { greater_than_or_equals_to: 0 }
 
+  before_destroy :has_employees?
+
   private
   def is_women_or_men_apparel
-    if self.mens_apparel == nil && self.womens_apparel && nil
-      erros.add(:mens_apparel,"You must assign at least one type of apparel they will sell")
+    if self.mens_apparel == nil && self.womens_apparel == nil
+      errors.add(:mens_apparel,"You must assign at least one type of apparel they will sell")
+    end
+  end
+  def has_employees?
+    begin
+      if self.employees.count > 0
+        raise ActiveRecord::Rollback, "There are employees still related to this store."
+      end
+      rescue => exception
+        puts exception
+        return false
     end
   end
 end
