fbafd97c82d15a893b8e902d283932dd3fc3da88
Bruno Cabral <bruno.cabral88@hotmail.com>
2017-04-11 18:25:36 +0000

    End of exercise 8 - step 4

--- lib/employee.rb
+++ lib/employee.rb
@@ -3,4 +3,11 @@ class Employee < ActiveRecord::Base
   
   validates :first_name, :last_name, :store_id, presence: true
   validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+
+  before_validation :define_user_password
+
+  private
+  def define_user_password
+    self.password = (0...8).map { (97 + rand(26)).chr }.join
+  end
 end
