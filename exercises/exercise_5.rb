2b4bee0f8680e5aaea93bd2f633895d6c12e495f
Bruno Cabral <bruno.cabral88@hotmail.com>
2017-04-11 16:37:18 +0000

    End of exercise 6

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Sum of total annual revenue"
+puts Store.sum(:annual_revenue)
+
+puts "Average annual revenue"
+puts Store.average(:annual_revenue)
+
+puts "# of stores generating over 1M annually"
+puts Store.where("annual_revenue > 1000000").count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Bruno", last_name: "Farias", hourly_rate: 40)
+@store1.employees.create(first_name: "Larry", last_name: "Page", hourly_rate: 30)
+@store1.employees.create(first_name: "Xunda", last_name: "Ferry", hourly_rate: 20)
+
+@store2 = Store.find_by(name: "Richmond")
+@store2.employees.create(first_name: "Niceman", last_name: "Thompson", hourly_rate: 30)
+@store2.employees.create(first_name: "Terry", last_name: "Crews", hourly_rate: 20)
